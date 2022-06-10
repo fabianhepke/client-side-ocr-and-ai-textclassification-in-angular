2da1c7337903e80212e116ad3298ad3df27b393b
Fabian Hepke <fabian.hepke@stp-online.de>
2022-06-10 14:17:30 +0200

    initial commit

--- ocr-ai-textclassification/karma.conf.js
+++ ocr-ai-textclassification/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ocr-ai-textxlassification'),
+      dir: require('path').join(__dirname, './coverage/ocr-ai-textclassification'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- ocr-ai-textclassification/src/app/app.component.spec.ts
+++ ocr-ai-textclassification/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ocr-ai-textxlassification'`, () => {
+  it(`should have as title 'ocr-ai-textclassification'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ocr-ai-textxlassification');
+    expect(app.title).toEqual('ocr-ai-textclassification');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ocr-ai-textxlassification app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ocr-ai-textclassification app is running!');
   });
 });
